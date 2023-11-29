3476250cbc7986eec0cd5a26a8743ff9f3a3e2f0
PedrorOliva <pedro.robson@zup.com.br>
2023-11-29 11:33:57 -0300

    refac testes validation

--- src/test/java/com/catalisa/gerenciadorDeTarefas/exception/ValidationTest.java
+++ src/test/java/com/catalisa/gerenciadorDeTarefas/exception/ValidationTest.java
@@ -20,85 +20,61 @@ public class ValidationTest {
 
   @Test
   public void testIsTitleValid() {
-    Validation validation = new Validation();
-
     assertTrue(validation.isTitleValid("Titulo"));
   }
 
   @Test
-  public void testIsTitleInvalid(){
-    Validation validation = new Validation();
-
+  public void testIsTitleInvalid() {
     assertThrows(HandleValidationField.class, () -> validation.isTitleValid(null));
   }
 
   @Test
   public void testIsTitleCountValid() {
-    Validation validation = new Validation();
-
     assertTrue(validation.isTittleCount("Titulo"));
   }
 
   @Test
   public void testIsTitleCountInvalid() {
-    Validation validation = new Validation();
-
     assertThrows(HandleValidationField.class, () -> validation.isTittleCount("titu"));
   }
 
   @Test
   public void testIsDescriptionValid() {
-    Validation validation = new Validation();
-
     assertTrue(validation.isDesciptionValid("descrição"));
   }
 
   @Test
   public void testIsDescriptionInvalid() {
-    Validation validation = new Validation();
-
     assertThrows(HandleValidationField.class, () -> validation.isDesciptionValid(null));
   }
 
   @Test
   public void testIsDescriptionCountValid() {
-    Validation validation = new Validation();
-
     assertTrue(validation.isDescriptionCount("descrição"));
   }
 
   @Test
   public void testIsDescriptionCountInvalid() {
-    Validation validation = new Validation();
-
     assertThrows(HandleValidationField.class, () -> validation.isDescriptionCount("de"));
   }
 
   @Test
   public void testIsValidityValid() {
-    Validation validation = new Validation();
-
     assertTrue(validation.isValidityValid(LocalDate.now().plusDays(1)));
   }
 
   @Test
   public void testIsValidityInvalid() {
-    Validation validation = new Validation();
-
     assertThrows(HandleValidationField.class, () -> validation.isValidityValid(null));
   }
 
   @Test
   public void testIsValidityAfterValid() {
-    Validation validation = new Validation();
-
     assertTrue(validation.isValidityAfter(LocalDate.now().plusDays(1)));
   }
 
   @Test
   public void testIsValidityAfterInvalid() {
-    Validation validation = new Validation();
-
     assertThrows(HandleValidationField.class, () -> validation.isValidityAfter(LocalDate.now().minusDays(1)));
   }
 }
